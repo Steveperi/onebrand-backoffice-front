041b28dc88e2cb1886defd4dc51b8815c66e6ba3
Steveperi <luras.periklis.1985@gmail.com>
2021-01-19 16:06:24 +0200

    test

---
+++ vue/dist/precache-manifest.741aadef2abc803a8dfc5b5f0ace7315.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99773289f1b8a475411",
+    "url": "/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "641e4c28a3fb0a0fe8be8b337a7bc6b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77768a168e7a528251f",
+    "url": "/js/about.80fdb2ae.js"
+  },
+  {
+    "revision": "b99773289f1b8a475411",
+    "url": "/js/app.2a1106d6.js"
+  },
+  {
+    "revision": "3369fcbe429b740ae591",
+    "url": "/js/chunk-vendors.4aa47481.js"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
